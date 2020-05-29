3c1852768ea992ece431d05f3c72534f16163d53
André Petermann <3175942+p3et@users.noreply.github.com>
2020-05-29 12:31:37 +0200

    queue comparison

---
+++ src/test/java/com/jambit/vavrdemo/QueueJava.java
@@ -0,0 +1,52 @@
+package com.jambit.vavrdemo;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import static org.junit.Assert.assertEquals;
+
+public class QueueJava {
+
+    @Test
+    public void test() {
+        final Queue<List<String>> queue = new LinkedList<>(List.of(List.of("alice@jambit.com", "bob@jambit.com"),
+                                                                   List.of("carol@jambit.com", "dave@jambit.com", "eve@jambit.com"),
+                                                                   List.of("frank@jambit.com")));
+
+        final var expectedOrder = List.of("alice@jambit.com",
+                                          "carol@jambit.com",
+                                          "frank@jambit.com",
+                                          "bob@jambit.com",
+                                          "dave@jambit.com",
+                                          "eve@jambit.com");
+
+        final var actualOrder = getNotificationOrder(queue);
+
+        assertEquals(expectedOrder, actualOrder);
+    }
+
+    private List<String> getNotificationOrder(final Queue<List<String>> queue) {
+        final List<String> order = new ArrayList<>();
+
+        List<String> current;
+        while ((current = queue.poll()) != null) {
+
+            final String head = current.get(0);
+            order.add(head);
+
+            if (current.size() > 1) {
+                final List<String> tail = current.subList(1, current.size());
+                queue.add(tail);
+            }
+        }
+
+        return order;
+    }
+
+
+}
---
+++ src/test/java/com/jambit/vavrdemo/QueueVavr.java
@@ -0,0 +1,48 @@
+package com.jambit.vavrdemo;
+
+
+import io.vavr.Tuple2;
+import io.vavr.collection.List;
+import io.vavr.collection.Queue;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class QueueVavr {
+
+    @Test
+    public void test() {
+        final Queue<List<String>> queue = Queue.of(List.of("alice@jambit.com", "bob@jambit.com"),
+                                                   List.of("carol@jambit.com", "dave@jambit.com", "eve@jambit.com"),
+                                                   List.of("frank@jambit.com"));
+
+        final List<String> expectedOrder = List.of("alice@jambit.com",
+                                                   "carol@jambit.com",
+                                                   "frank@jambit.com",
+                                                   "bob@jambit.com",
+                                                   "dave@jambit.com",
+                                                   "eve@jambit.com");
+
+        final List<String> actualOrder = getNotificationOrder(queue);
+
+        assertEquals(expectedOrder, actualOrder);
+    }
+
+    private List<String> getNotificationOrder(final Queue<List<String>> queue) {
+        final Tuple2<List<String>, Queue<List<String>>> currentRemainingQueuePair = queue.dequeue();
+
+        final List<String> current = currentRemainingQueuePair._1;
+        final Queue<List<String>> remainingQueue = currentRemainingQueuePair._2;
+
+        final String head = current.head();
+        final List<String> tail = current.tail();
+
+        final Queue<List<String>> updatedQueue = tail.isEmpty() ? remainingQueue
+                                                                : remainingQueue.enqueue(tail);
+
+        return updatedQueue.isEmpty() ? List.of(head)
+                                      : getNotificationOrder(updatedQueue).prepend(head);
+    }
+
+
+}
