0cc38ee8534f0540a3873ff5632da12a8be772f2
André Petermann <3175942+p3et@users.noreply.github.com>
2020-06-02 11:28:30 +0200

    tuples

--- src/test/java/com/jambit/vavrdemo/Tuples.java
+++ src/test/java/com/jambit/vavrdemo/Tuples.java
@@ -10,7 +10,20 @@ import static org.junit.Assert.assertEquals;
 public class Tuples {
 
     @Test
-    public void inAction() {
+    public void mapAndApply() {
+        final Tuple2<String, String> original = Tuple.of("foo", "bar");
+        final Tuple2<String, String> mapped = original.map(f1 -> f1 + f1, f2 -> f2)
+                                                      .map((f1, f2) -> Tuple.of(f1 + f2, "baz"));
+
+        assertEquals(Tuple.of("foofoobar", "baz"), mapped);
+
+        final int length = mapped.apply((f1, f2) -> f1.length() + f2.length());
+
+        assertEquals(12, length);
+    }
+
+    @Test
+    public void concatAndUpdate() {
         final Tuple2<String, Integer> foo = Tuple.of("foo", 1);
         final Tuple3<String, Integer, Boolean> bar = Tuple.of("bar", 2, true);
 
@@ -22,5 +35,4 @@ public class Tuples {
 
         assertEquals(Tuple.of(2, "foo", "bar", 4, true, false), result);
     }
-
 }
